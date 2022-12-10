284ef9f609a2a7484e85d289bc4f5b9be99a7b81
DULAC Philippe <phd0@hotmail.com>
2022-12-10 09:57:01 +0100

    day08
    not finished

--- day08.py
+++ day08.py
@@ -1,73 +1,91 @@
 import sys
 from collections import defaultdict
 
-infile = sys.argv[1] if len(sys.argv) > 1 else 'day08.txt'
+infile = sys.argv[1] if len(sys.argv) > 1 else 'day08a.txt'
 data = open(infile).read().strip()
-linesH = [x for x in data.split('\n')]
-linesV = []
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+lines = [x for x in data.split('\n')]
 
-
-def printresults():
-    if (len(linesV) < 10):
-        print(linesH, linesV)
-
-
-for h in range(len(linesH)):
-    linesV.append('')
-
-# formation des lignes V
-for h in range(len(linesH)):
-    for i in range(len(linesH[0])):
-        linesV[h] = linesV[h] + linesH[i][h]
-        pass
-
-print('0:', 2*len(linesH)+2*len(linesV)-4)
+# print('0:', 2*len(lines)+2*len(lines[0])-4) # uniquement les bords
 result = 0
-printresults()
+memory = []
+# partie 1
+for y in range(len(lines)-1, 0, -1):
+    max = -1
+    for x in range(0, len(lines[0])-1):
+        if (int(lines[y][x]) > max):
+            max = int(lines[y][x])
+            if ([y, x] not in memory):
+                result += 1
+                memory.append([y, x])
 
-for cc in range(0, len(linesH)-1):
+for y in range(0, len(lines)-1):
     max = -1
-    for h in range(0, len(linesH[0])-1):
-        if (linesV[cc][h] not in numbers):
-            break
-        if (int(linesH[cc][h]) > max):
-            result += 1
-            max = int(linesH[cc][h])
-            linesH[cc] = linesH[cc][:h] + '>' + linesH[cc][h+1:]
-            linesV[h] = linesV[h][:cc] + '>' + linesV[h][cc+1:]
-for cc in range(len(linesH)-1, 1, -1):
-    max = -1
-    for h in range(len(linesH[0])-1, 0, -1):
-        if (linesV[cc][h] not in numbers):
-            break
-        if (int(linesH[cc][h]) > max):
-            result += 1
-            max = int(linesH[cc][h])
-            linesH[cc] = linesH[cc][:h] + '<' + linesH[cc][h+1:]
-            linesV[h] = linesV[h][:cc] + '<' + linesV[h][cc+1:]
+    for x in range(len(lines[0])-1, 0, -1):
+        if (int(lines[y][x]) > max):
+            max = int(lines[y][x])
+            if ([y, x] not in memory):
+                result += 1
+                memory.append([y, x])
 
-printresults()
+for x in range(0, len(lines[0])-1):
+    max = -1
+    for y in range(0, len(lines)-1):
+        if (int(lines[y][x]) > max):
+            max = int(lines[y][x])
+            if ([y, x] not in memory):
+                result += 1
+                memory.append([y, x])
 
-for cc in range(0, len(linesV)-1):
+for x in range(len(lines[0])-1, 0, -1):
     max = -1
-    for v in range(0, len(linesV[0])-1):
-        if (linesV[cc][v] not in numbers):
-            break
-        if (int(linesV[cc][v]) > max):
-            result += 1
-            max = int(linesV[cc][v])
-            linesV[cc] = linesV[cc][:v] + 'v' + linesV[cc][v+1:]
-            linesH[v] = linesH[v][:cc] + 'v' + linesH[v][cc+1:]
-for cc in range(len(linesV)-1, 0, -1):
-    max = -1
-    for v in range(len(linesV[0])-1, 0, -1):
-        if (linesV[cc][v] not in numbers):
-            break
-        if (int(linesV[cc][v]) > max):
-            result += 1
-            max = int(linesV[cc][v])
-            linesV[cc] = linesV[cc][:v] + '^' + linesV[cc][v+1:]
-            linesH[v] = linesH[v][:cc] + '^' + linesH[v][cc+1:]
-printresults()
+    for y in range(len(lines)-1, 0, -1):
+        if (int(lines[y][x]) > max):
+            max = int(lines[y][x])
+            if ([y, x] not in memory):
+                result += 1
+                memory.append([y, x])
+
 print('1:', result)
+# partie 2
+result = 0
+memory = []
+for y in range(len(lines)-1, 0, -1):
+    for x in range(0, len(lines[0])):
+        max = 1
+        cc = 1
+        for yy in range(len(lines[y])-1, y, -1):
+            if lines[yy][x] < lines[y][x]:
+                cc = 1
+                break
+            else:
+                cc += 1
+                #print(cc, '/', x, ',', yy, '< ', lines[yy][x], sep='')
+        max *= cc
+        for yy in range(y-1, -1, -1):
+            if lines[yy][x] < lines[y][x]:
+                cc = 1
+                break
+            else:
+                cc += 1
+                #print(cc, '/', x, ',', yy, '> ', lines[yy][x], sep='')
+        max *= cc
+        for xx in range(len(lines[x])-1, x, -1):
+            if lines[y][xx] < lines[y][x]:
+                cc = 1
+                break
+            else:
+                cc += 1
+                #print(cc, '/', xx, ',', y, '< ', lines[y][xx], sep='')
+        max *= cc
+        for xx in range(x-1, -1, -1):
+            if lines[y][xx] < lines[y][x]:
+                cc = 1
+                break
+            else:
+                cc += 1
+                #print(cc, '/', xx, ',', y, '> ', lines[y][xx], sep='')
+        max *= cc
+    if (max > result):
+        result = max
+
+print('2:', result)
