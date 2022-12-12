49f7f02025a865fbaf6a5d26bfb9c7ad103e034c
DULAC Philippe <phd0@hotmail.com>
2022-12-12 17:10:13 +0100

    day10

--- day10.py
+++ day10.py
@@ -5,28 +5,62 @@ data = open(infile).read().strip()
 lines = [x for x in data.split('\n')]
 resultat = 0
 value = 0
-lastValue = 0
 indexCycle = 0
-total = 1
+total = 0
 cycle = 0
-cycles = [20, 60, 100, 140, 180, 220, 9999]
+steps = [20, 60, 100, 140, 180, 220, 9999]
 op = ''
+CRT = ''
+spritePosition = '###.......................'
+resultatTest = '##..##..##..##..##..##..##..##..##..##..###...###...###...###...###...###...###.####....####....####....####....####....#####.....#####.....#####.....#####.....######......######......######......###########.......#######.......#######.....'
+# for i in range(240):   CRT += '.'
 
+
+def printSpritePosition(c):
+    global spritePosition, cycle, total, line, infile
+    #print(spritePosition, ' total=', total, '\tcycle=', cycle,  '  \t', line, ' ', sep='')
+    print(spritePosition[0:(cycle % 40)] + (c if (spritePosition[cycle % 40] == '#') else '!') +
+          spritePosition[(cycle % 40)+1:40], ' total=', total, '\tcycle=', cycle,  '  \t', line, ' ', sep='')
+
+
+print('0123456789012345678901234567890123456789')
 for line in lines:
+    spritePosition = ''
+    for i in range(40):
+        spritePosition += '.'
     try:
         op, value = line.split(' ')
-        cycle += 2
+        value = int(value)
+
+        if cycle >= steps[indexCycle]:
+            resultat += total * steps[indexCycle]
+            # print(indexCycle, steps[indexCycle], ',', cycle)
+            indexCycle += 1
+        # calcul de spritePosition
+
+        spritePosition = spritePosition[0:total if total > 0 else 0] + \
+            '###' + spritePosition[1+(total if total >= 0 else 0):len(spritePosition)]
+        CRT = CRT+spritePosition[cycle % 40]
+        printSpritePosition('1')
+        cycle += 1
+        CRT = CRT+spritePosition[cycle % 40]
+        printSpritePosition('2')
+        cycle += 1
     except:
+        spritePosition = spritePosition[0:total if total > 0 else 0] + \
+            '###' + spritePosition[1+(total if total >= 0 else 0):len(spritePosition)]
+        CRT = CRT+spritePosition[cycle % 40]
+        printSpritePosition('x')
         cycle += 1
         value = 0
-    value = int(value)
-    if (op != 'noop'):
-        lastValue = value
-    if cycle >= cycles[indexCycle]:
-        resultat += total * cycles[indexCycle]
-        #print(indexCycle, cycles[indexCycle], ',', cycle)
-        indexCycle += 1
-        pass
     total += value
+    if (CRT != resultatTest[:len(CRT)] and infile == 'day10a.txt'):
+        print()
+        print(CRT+'<')
+        print(resultatTest[:len(CRT)])
+        print('---------')
+        break
 
 print('1:', resultat)
+for i in range(0, 240, 40):
+    print(CRT[i:i+40])
